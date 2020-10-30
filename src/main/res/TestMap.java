168f9ad4c6556748eb5a3ff6f622d126d805eba2
Alex Mark <allxundermark@gmail.com>
2020-10-30 18:34:44 +0200

    три

--- src/main/res/TestMap.java
+++ src/main/res/TestMap.java
@@ -6,96 +6,33 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class TestMap implements Serializable {
-    public Map<Integer, Players> playersMap = new Map<Integer, Players>() {
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return false;
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return false;
-        }
-
-        @Override
-        public Players get(Object key) {
-            return null;
-        }
-
-        @Nullable
-        @Override
-        public Players put(Integer key, Players value) {
-            return null;
-        }
-
-        @Override
-        public Players remove(Object key) {
-            return null;
-        }
-
-        @Override
-        public void putAll(@NotNull Map<? extends Integer, ? extends Players> m) {
-
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @NotNull
-        @Override
-        public Set<Integer> keySet() {
-            return null;
-        }
-
-        @NotNull
-        @Override
-        public Collection<Players> values() {
-            return null;
-        }
-
-        @NotNull
-        @Override
-        public Set<Entry<Integer, Players>> entrySet() {
-            return null;
-        }
-    };
+    public Map<String, Players> playersMap = new HashMap<>();
 
 
     public TestMap() {
 
-        playersMap.put(1, new Players("Роман", "Боровик", "Захисник", (double) 25, "ФК Юність"));
-        playersMap.put(2, new Players("Владислав", "Павлов", "Півзахисник", (double) 24, "ФК Юність"));
+        playersMap.put("p1", new Players("Роман", "Боровик", "Захисник", (double) 25, "ФК Юність"));
+        playersMap.put("p2", new Players("Владислав", "Павлов", "Півзахисник", (double) 24, "ФК Юність"));
     }
 
 
-    public Map<Integer, Players> createMap() {
+    /*public Map<String, Players> createMap() {
         playersMap.put(1, new Players("Роман", "Боровик", "Захисник", (double) 25, "ФК Юність"));
         playersMap.put(2, new Players("Владислав", "Павлов", "Півзахисник", (double) 24, "ФК Юність"));
         return playersMap;
-    }
+    }*/
 
-    public Map<Integer, Players> getPlayersMap() {
+    public Map<String, Players> getPlayersMap() {
 
         return playersMap;
     }
 
-    public void setPlayersMap(Map<Integer, Players> playersMap) {
+    public void setPlayersMap(Map<String, Players> playersMap) {
         this.playersMap = playersMap;
     }
 }
--- web/Command.jsp
+++ web/Command.jsp
@@ -7,6 +7,8 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
@@ -80,6 +82,7 @@
                     <tr>
                         <td>
                                  ${age}
+                            ${player.playersMap.p1.name}
 
 
                         </td>
